49ff9728b62684ed5d05e06bfd149e18563b0ade
HarrisonPowell <harrisonpowell365@gmail.com>
2020-05-06 17:01:35 +1000

    devise all setup with models ready to push

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,7 @@
 class BlogsController < ApplicationController
   
+  before_action :set_blog, only: [:show]
+  before_action :set_user_blog, only: [:edit, :update, :destroy]
   before_action :authenticate_user!
   
   def index
