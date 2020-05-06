59edb500e90b78b0b40d40a9b6f5637c944b585f
GARY <gary.v.ho@gmail.com>
2020-05-06 16:11:50 +1000

    typo fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     get '/blogs/:id', to: 'blogs', as: 'blog'
     put '/blogs/:id', to: 'blogs#update'
     patch '/blogs/:id', to: 'blogs#update'
-    delete '/blogs/:id', to: 'blogs#destroy', as: 'destroy_blog'  get '/blogs/:id/edit', to: 'blogs#edit', as: 'edit_blog'
+    delete '/blogs/:id', to: 'blogs#destroy', as: 'destroy_blog'
     get '/blogs/:id/edit', to: 'blogs#edit', as: 'edit_blog'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
