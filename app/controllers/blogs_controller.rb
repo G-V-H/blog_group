e93abd0069c81397a798c05ee862b443f4b2aa9b
HarrisonPowell <harrisonpowell365@gmail.com>
2020-05-06 16:19:33 +1000

    need  models to be setup

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,7 @@
 class BlogsController < ApplicationController
+  
+  before_action :authenticate_user!
+  
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+    devise_for :users
+    root "home#index"
+
     get '/blogs', to: 'blogs#index', as: 'blogs'
     post '/blogs', to: 'blogs#create'
     get '/blogs/new', to: 'blogs#new', as: 'new_blog'
@@ -7,5 +10,5 @@ Rails.application.routes.draw do
     patch '/blogs/:id', to: 'blogs#update'
     delete '/blogs/:id', to: 'blogs#destroy', as: 'destroy_blog'  get '/blogs/:id/edit', to: 'blogs#edit', as: 'edit_blog'
     get '/blogs/:id/edit', to: 'blogs#edit', as: 'edit_blog'
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    
   end
