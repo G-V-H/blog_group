da7eb987a5ade058f15678f6f5825569814dde89
GARY <gary.v.ho@gmail.com>
2020-05-06 15:30:15 +1000

    blog routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'blogs/index'
-  get 'blogs/show'
-  get 'blogs/new'
-  get 'blogs/create'
-  get 'blogs/edit'
-  get 'blogs/destroy'
+  get '/blogs', to: 'blogs#index', as: 'blogs'
+  post '/blogs', to: 'blogs#create'
+  get '/blogs/new', to: 'blogs#new', as: 'new_blog'
+  get '/blogs/:id', to: 'blogs', as: 'blog'
+  put '/blogs/:id', to: 'blogs#update'
+  patch '/blogs/:id', to: 'blogs#update'
+  delete '/blogs/:id', to: 'blogs#destroy', as: 'destroy_blog'  get '/blogs/:id/edit', to: 'blogs#edit', as: 'edit_blog'
+  get '/blogs/:id/edit', to: 'blogs#edit', as: 'edit_blog'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
