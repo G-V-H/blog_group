0d5d3eaf0cb845f5ea2a7fd4179d39806be7c8c8
patricia <patriciapavia236@gmail.com>
2020-05-06 21:40:23 +1000

    add style to form

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,9 @@
 class BlogsController < ApplicationController
+  
+  before_action :set_blog, only: [:show]
+  before_action :set_user_blog, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
+  
   def index
   end
 
