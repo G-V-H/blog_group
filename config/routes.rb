6e31a3d8d719e4c7e37b7b1c3905abf28f1a2a2b
patricia <patriciapavia236@gmail.com>
2020-05-06 16:37:15 +1000

    add model

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     get '/blogs', to: 'blogs#index', as: 'blogs'
     post '/blogs', to: 'blogs#create'
     get '/blogs/new', to: 'blogs#new', as: 'new_blog'
-    get '/blogs/:id', to: 'blogs', as: 'blog'
+    get '/blogs/:id', to: 'blogs#show', as: 'blog'
     put '/blogs/:id', to: 'blogs#update'
     patch '/blogs/:id', to: 'blogs#update'
     delete '/blogs/:id', to: 'blogs#destroy', as: 'destroy_blog'
