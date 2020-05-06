8063274d9c254c48a0c3fee55e0a778d9e3d440d
Harrison Powell <61393945+Harrisonp365@users.noreply.github.com>
2020-05-06 15:41:49 +1000

    Revert "added model"

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-
+  get '/blogs', to: 'blogs#index', as: 'blogs'
+  post '/blogs', to: 'blogs#create'
+  get '/blogs/new', to: 'blogs#new', as: 'new_blog'
+  get '/blogs/:id', to: 'blogs', as: 'blog'
+  put '/blogs/:id', to: 'blogs#update'
+  patch '/blogs/:id', to: 'blogs#update'
+  delete '/blogs/:id', to: 'blogs#destroy', as: 'destroy_blog'  get '/blogs/:id/edit', to: 'blogs#edit', as: 'edit_blog'
+  get '/blogs/:id/edit', to: 'blogs#edit', as: 'edit_blog'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
