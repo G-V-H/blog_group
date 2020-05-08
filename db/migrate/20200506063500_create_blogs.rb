25f867059cca0f765e3151c7a88574a75211ecca
HarrisonPowell <harrisonpowell365@gmail.com>
2020-05-08 14:09:02 +1000

    hot fix

--- db/migrate/20200506063500_create_blogs.rb
+++ db/migrate/20200506063500_create_blogs.rb
@@ -3,7 +3,6 @@ class CreateBlogs < ActiveRecord::Migration[5.2]
     create_table :blogs do |t|
       t.string :title
       t.text :description
-      t.text :comment
 
       t.timestamps
     end
